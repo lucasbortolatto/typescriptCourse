74a035e0378179b16bdd73917ded690763bc62ed
Luidson Lucas <lucasbortolatto2@hotmail.com>
2016-12-22 20:36:55 -0200

    Exercise Correction

--- exercises.ts
+++ exercises.ts
@@ -1,30 +1,29 @@
 // Exercise 1
-var double = (value) => value * 2;
+const double = (value: number) => value * 2;
 console.log(double(10));
 
 // Exercise 2
-var greet = function (name = "Ze") {
-    if (name === undefined) { name = "Max"; }
+const greet = function (name = "Ze") {
     console.log("Hello, " + name);
 };
 greet();
 greet("Anna");
 
 // Exercise 3
-var numbers = [-3, 33, 38, 5];
+const numbers = [-3, 33, 38, 5];
 console.log(Math.min(...numbers));
 
 // Exercise 4
-var newArray = [55, 20];
-Array.prototype.push.apply(newArray, numbers);
+const newArray = [55, 20];
+newArray.push(...numbers);
 console.log(newArray);
 
 // Exercise 5
-var testResults = [3.89, 2.99, 1.38];
-var [result1, result2, result3] = testResults;
+const testResults = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults;
 console.log(result1, result2, result3);
 
 // Exercise 6
-var scientist = {firstName: "Will", experience: 12};
-var {firstName, experience } = scientist;
+const scientist = {firstName: "Will", experience: 12};
+const {firstName, experience } = scientist;
 console.log(firstName, experience);
