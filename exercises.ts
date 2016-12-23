599a569d6bb75e7be874ad7c2c9896a86cdde8b5
Luidson Lucas <lucasbortolatto2@hotmail.com>
2016-12-22 23:02:35 -0200

    Exercises to enhance in exercises.ts

--- exercises.ts
+++ exercises.ts
@@ -2,18 +2,21 @@
 function Car(name) {
     this.name = name;
     this.acceleration = 0;
-    this.honk = function () {
+
+    this.honk = function() {
         console.log("Toooooooooot!");
     };
-    this.accelerate = function (speed) {
+
+    this.accelerate = function(speed) {
         this.acceleration = this.acceleration + speed;
-    };
+    }
 }
 var car = new Car("BMW");
 car.honk();
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
+
 // Exercise 2
 var baseObject = {
     width: 0,
@@ -22,10 +25,11 @@ var baseObject = {
 var rectangle = Object.create(baseObject);
 rectangle.width = 5;
 rectangle.length = 2;
-rectangle.calcSize = function () {
+rectangle.calcSize = function() {
     return this.width * this.length;
 };
 console.log(rectangle.calcSize());
+
 // Exercise 3
 var person = {
     _firstName: ""
