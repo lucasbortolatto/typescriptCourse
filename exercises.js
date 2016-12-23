f201469a158f4d2e6ee44c4c4225eb6c595fbc5b
Luidson Lucas <lucasbortolatto2@hotmail.com>
2016-12-22 23:51:39 -0200

    Exercises Solved

--- exercises.js
+++ exercises.js
@@ -1,52 +1,70 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 // Exercise 1
-function Car(name) {
-    this.name = name;
-    this.acceleration = 0;
-    this.honk = function () {
-        console.log("Toooooooooot!");
-    };
-    this.accelerate = function (speed) {
+var Car = (function () {
+    function Car(name) {
+        this.name = name;
+        this.acceleration = 0;
+    }
+    Car.prototype.accelerate = function (speed) {
         this.acceleration = this.acceleration + speed;
     };
-}
-var car = new Car("BMW");
+    Car.prototype.honk = function () {
+        console.log("Toooooooooot!");
+    };
+    return Car;
+}());
+var car = new Car("PEUGEOT");
 car.honk();
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
 // Exercise 2
-var baseObject = {
-    width: 0,
-    length: 0
-};
-var rectangle = Object.create(baseObject);
-rectangle.width = 5;
-rectangle.length = 2;
-rectangle.calcSize = function () {
-    return this.width * this.length;
-};
-console.log(rectangle.calcSize());
+var BaseObject = (function () {
+    function BaseObject(width, length) {
+        this.width = 0;
+        this.length = 0;
+        this.width = width;
+        this.length = length;
+    }
+    BaseObject.prototype.calcArea = function () {
+        return this.width * this.length;
+    };
+    return BaseObject;
+}());
+var Rectangle = (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super.apply(this, arguments) || this;
+    }
+    return Rectangle;
+}(BaseObject));
+var rectangle = new Rectangle(2, 2);
+console.log(rectangle.calcArea());
 // Exercise 3
-var person = {
-    _firstName: ""
-};
-Object.defineProperty(person, "firstName", {
-    get: function () {
+var Person = (function () {
+    function Person() {
+        this._firstName = "";
+    }
+    Person.prototype.getFirstName = function () {
         return this._firstName;
-    },
-    set: function (value) {
+    };
+    Person.prototype.setFirstName = function (value) {
         if (value.length > 3) {
             this._firstName = value;
         }
         else {
             this._firstName = "";
         }
-    },
-    enumerable: true,
-    configurable: true
-});
-console.log(person.firstName);
-person.firstName = "Ma";
-console.log(person.firstName);
-person.firstName = "Maximilian";
-console.log(person.firstName);
+    };
+    return Person;
+}());
+var person = new Person();
+console.log(person.getFirstName());
+person.setFirstName("Lu");
+console.log(person.getFirstName());
+person.setFirstName("Lucas");
+console.log(person.getFirstName());
--- exercises.ts
+++ exercises.ts
@@ -1,56 +1,77 @@
 // Exercise 1
-function Car(name) {
-    this.name = name;
-    this.acceleration = 0;
+class Car{
+    name: string;
+    acceleration: number;
 
-    this.honk = function() {
-        console.log("Toooooooooot!");
-    };
+    constructor(name: string){
+        this.name = name;
+        this.acceleration = 0;
+    }
 
-    this.accelerate = function(speed) {
+    accelerate(speed: number){
         this.acceleration = this.acceleration + speed;
     }
+
+    honk(){
+            console.log("Toooooooooot!");
+    }
+    
 }
-var car = new Car("BMW");
+
+var car = new Car("PEUGEOT");
 car.honk();
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
 
 // Exercise 2
-var baseObject = {
-    width: 0,
-    length: 0
-};
-var rectangle = Object.create(baseObject);
-rectangle.width = 5;
-rectangle.length = 2;
-rectangle.calcSize = function() {
-    return this.width * this.length;
-};
-console.log(rectangle.calcSize());
+
+class BaseObject{
+    width: number = 0;
+    length: number = 0;
+
+    constructor(width: number, length: number){
+        this.width = width;
+        this.length = length;
+    }
+
+    calcArea(){
+        return this.width * this.length;
+
+    }
+
+}
+
+class Rectangle extends BaseObject {
+
+}
+
+var rectangle = new Rectangle(2,2);
+console.log(rectangle.calcArea());
 
 // Exercise 3
-var person = {
-    _firstName: ""
-};
-Object.defineProperty(person, "firstName", {
-    get: function () {
+
+class Person {
+    _firstName: string = "";
+
+    getFirstName(){
         return this._firstName;
-    },
-    set: function (value) {
+    }
+
+    setFirstName(value: string){
         if (value.length > 3) {
             this._firstName = value;
         }
         else {
             this._firstName = "";
         }
-    },
-    enumerable: true,
-    configurable: true
-});
-console.log(person.firstName);
-person.firstName = "Ma";
-console.log(person.firstName);
-person.firstName = "Maximilian";
-console.log(person.firstName);
+    }
+}
+var person = new Person();
+console.log(person.getFirstName());
+person.setFirstName("Lu");
+console.log(person.getFirstName());
+person.setFirstName("Lucas");
+console.log(person.getFirstName());
+
+
